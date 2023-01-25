af52e501d1d2ce53cb4574a85dc06e1dc5a6a679
Ashton Ruiz <ashton.ruiz@yahoo.com>
2023-01-24 16:24:38 -0800

    fixed 404 image problem

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -4,7 +4,9 @@ export default function NotFound() {
   return (
     <div>
       <p>Not Found</p>
-      <img src='/public/404.jpg'/>
+      <img src='https://http.cat/404'/>
     </div>
   );
 }
+
+// Why can't I use a photo that is in public?? /public/error.png
