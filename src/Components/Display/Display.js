aeabb84be1d56e0b5f700648259b5f0c25aa7194
Ashton Ruiz <ashton.ruiz@yahoo.com>
2023-01-24 16:53:38 -0800

    got the image to work for a second and now it's broken again. tried resizing still no worky. Going to work on styling now

--- src/Components/Display/Display.js
+++ src/Components/Display/Display.js
@@ -5,6 +5,6 @@ export default function Display() {
   const { red, green, blue } = useParams();
 
   return (
-    <div style={{ backgroundImage: `rbg${red}, ${green}, ${blue}` }}>RGB({ red }, { green }, { blue })</div>
+    <div style={{ backgroundColor: `rgb(${red}, ${green}, ${blue})` }}>RGB({ red }, { green }, { blue })</div>
   );
 }
--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -3,10 +3,8 @@ import React from 'react';
 export default function NotFound() {
   return (
     <div>
-      <p>Not Found</p>
-      <img src='https://http.cat/404'/>
+      <img className='error-img' src='../Images/error.png'/>
     </div>
   );
 }
 
-// Why can't I use a photo that is in public?? /public/error.png
